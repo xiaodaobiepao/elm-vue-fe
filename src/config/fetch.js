d57bf3593b58ebb20675ffc94712c82b927bba34
xiaodaobiepao <1310851368@qq.com>
2019-08-27 17:03:44 +0800

    newfe

---
+++ src/config/fetch.js
@@ -0,0 +1,89 @@
+import {
+	baseUrl
+} from './env'
+
+export default async(url = '', data = {}, type = 'GET', method = 'fetch') => {
+	type = type.toUpperCase();
+	url = baseUrl + url;
+
+	if (type == 'GET') {
+		let dataStr = ''; //数据拼接字符串
+		Object.keys(data).forEach(key => {
+			dataStr += key + '=' + data[key] + '&';
+		})
+
+		if (dataStr !== '') {
+			dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'));
+			url = url + '?' + dataStr;
+		}
+	}
+
+	if (window.fetch && method == 'fetch') {
+		let requestConfig = {
+			credentials: 'include',
+			method: type,
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			mode: "cors",
+			cache: "force-cache"
+		}
+
+		if (type == 'POST') {
+			Object.defineProperty(requestConfig, 'body', {
+				value: JSON.stringify(data)
+			})
+		}
+    let response
+    let responseJson
+		try {
+      response = await fetch(url, requestConfig);
+      if (response.ok === false) {
+        throw new Error(response.statusText)
+      }
+		} catch (error) {
+      console.log(error)
+			throw new Error(error)
+    }
+    try {
+      responseJson = await response.json();
+      console.log(responseJson)
+      return responseJson
+    } catch (e) {
+      console.log(e)
+    }
+	} else {
+		return new Promise((resolve, reject) => {
+			let requestObj;
+			if (window.XMLHttpRequest) {
+				requestObj = new XMLHttpRequest();
+			} else {
+				requestObj = new ActiveXObject;
+			}
+
+			let sendData = '';
+			if (type == 'POST') {
+				sendData = JSON.stringify(data);
+			}
+
+			requestObj.open(type, url, true);
+			requestObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			requestObj.send(sendData);
+
+			requestObj.onreadystatechange = () => {
+				if (requestObj.readyState == 4) {
+					if (requestObj.status == 200) {
+						let obj = requestObj.response
+						if (typeof obj !== 'object') {
+							obj = JSON.parse(obj);
+						}
+						resolve(obj)
+					} else {
+						reject(requestObj)
+					}
+				}
+			}
+		})
+	}
+}
