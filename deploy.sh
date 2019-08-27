a50a65c846ff611f0a44d109f93b1ec460fb8a3d
xiaodaobiepao <1310851368@qq.com>
2019-08-27 17:38:53 +0800

    no -f

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ git pull
 yarn
 yarn run build
 rm -rf /root/koa-elm/public/elm
+rm -rf /root/koa-elm/public/index.html
 cp -r elm /root/koa-elm/public/elm
 mv /root/koa-elm/public/elm/index.html /root/koa-elm/public/
